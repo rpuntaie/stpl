b84f7f37f42ce0fb7cbd0cd2b608e1a01ab73b01
Roland Puntaier <roland.puntaier@chello.at>
2018-09-07 22:21:31 +0200

    reinstalled cmd line tests

--- test_stpl.py
+++ test_stpl.py
@@ -367,3 +367,81 @@ class TestSTPLDir(unittest.TestCase):
         ''', result='''
             [1, 3, 5]
         ''')
+
+
+import pytest
+from stpl import main
+import os
+from subprocess import Popen, PIPE
+import sys
+
+from mock import patch
+
+if 'win' in sys.platform:
+    os.environ['PATH']+=";"+os.path.dirname(__file__)
+else:
+    os.environ['PATH']+=":"+os.path.dirname(__file__)
+
+@pytest.yield_fixture
+def tmpworkdir(tmpdir):
+    cwd = os.getcwd()
+    os.chdir(tmpdir.strpath)
+    yield tmpdir
+    os.chdir(cwd)
+
+def test_string(request,capfd):
+    main(file_or_string="{{i+j}}",directory=None,code="i=2;j=3")
+    out, err = capfd.readouterr()
+    assert out == '5'
+
+@pytest.yield_fixture
+def stplfile(tmpworkdir):
+    with open('tst.c.stpl','w',encoding='utf-8') as f:
+        f.write('''\
+{{i+j}}'''
+        )
+    yield 'tst.c.stpl'
+
+def test_file(stplfile,capfd):
+    main(file_or_string=stplfile,directory=None,code="i=2;j=3")
+    out, err = capfd.readouterr()
+    assert out == '5'
+
+def test_dir(stplfile):
+    main(file_or_string=stplfile,directory='tst.c',code="i=2;j=3")
+    assert os.path.isfile('tst.c')
+    assert open('tst.c').read()=='5'
+
+@pytest.yield_fixture
+def nostplfile(tmpworkdir):
+    with open('tst.c','w',encoding='utf-8') as f:
+        f.write('''\
+{{i+j}}'''
+        )
+    yield 'tst.c'
+
+#like inplace
+def test_nostpl(nostplfile):
+    main(file_or_string=nostplfile,directory='.',code="i=2;j=3")
+    assert os.path.isfile('tst.c')
+    assert open('tst.c').read()=='5'
+
+def test_parse_args(stplfile):
+    testargs = ['stpl.py',stplfile,'.','i=2;j=3']
+    with patch.object(sys, 'argv', testargs):
+        main()
+        assert os.path.isfile('tst.c')
+        assert open('tst.c').read()=='5'
+
+def test_raise(request):
+    with pytest.raises(ValueError):
+        main(file_or_string="{{i+j}}",directory='.',code="i=2;j=3")
+
+def test_stdin(request):
+    p=Popen(['stpl','-','-','i=2;j=3'],stdin=PIPE,stdout=PIPE)
+    p.stdin.write(b'{{i+j}}\n')
+    p.stdin.close()
+    p.wait()
+    res=p.stdout.read()
+    assert res==b'5\n'
+
